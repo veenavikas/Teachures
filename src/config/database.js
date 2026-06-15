3941fc4bc68e05d650d6fe2010c30ca198b42af8
veenavikas <veenavikas17@gmail.com>
2026-06-15 23:32:30 +0530

    fix: sanitize DATABASE_URL from hostinger backslashes

--- src/config/database.js
+++ src/config/database.js
@@ -1,5 +1,10 @@
 const { PrismaClient } = require('@prisma/client');
 
+// Hostinger's environment variable manager sometimes adds unwanted backslashes before special characters
+if (process.env.DATABASE_URL) {
+    process.env.DATABASE_URL = process.env.DATABASE_URL.replace(/\\%/g, '%').replace(/\\/g, '');
+}
+
 const prisma = new PrismaClient();
 
 module.exports = prisma;
